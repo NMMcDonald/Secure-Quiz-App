a7dff2e194650bd056b6ed27505888af838cbcdd
NMMcDonald <nicolasmmcdonald>
2022-04-21 10:17:30 -0700

    some python

--- webapp.py
+++ webapp.py
@@ -1,1 +1,18 @@
+import os
 
+app = Flask(__name__)
+
+app.secret_key=os.environ["SECRET_KEY"];
+
+@app.route('/')
+def renderMain():
+    return render_template('home.html')
+
+@app.route('/retakeQuiz')
+def retakeQuiz():
+    session.clear()
+    return redirect(url_for('renderMain'))
+
+@app.route('/page1')
+def renderPage1():
+    return render_template('page1.html')
